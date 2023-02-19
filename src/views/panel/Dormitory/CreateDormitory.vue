fd42b3ec2a461f01f6c6ad485a70b330c4dd63df
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-19 19:38:24 +0300

    More pages added

---
+++ src/views/panel/Dormitory/CreateDormitory.vue
@@ -0,0 +1,74 @@
+<template>
+<div class="card border-success mb-4">
+	<div class="card-header">
+		Create Dormitory
+	</div>
+
+	<div id="ialert" class="" role="alert"></div>
+	<form method="post">
+    <div class="card-body">
+
+        <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="title" name="title" placeholder="Title..." required autofocus autocomplete="off">
+        <label for="title">Title</label>
+        <div class="invalid-feedback"></div>
+        </div>
+
+        <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="phones" name="phones" placeholder="Phones..." required autocomplete="off">
+        <label for="phones">Phones</label>
+        <div class="invalid-feedback"></div>
+        </div>
+
+        <div class="form-floating mb-3">
+            <select name="city" id="city" class="form-control">
+                <option>Choose City</option>
+                <option value="1">City 1</option>
+            </select>
+            <label for="city">City</label>
+            <div class="invalid-feedback"></div>
+        </div>
+
+        <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="location" name="location" placeholder="Location..." required autocomplete="off">
+        <label for="location">Location</label>
+        <div class="invalid-feedback"></div>
+        </div>
+
+        <div class="form-floating mb-3">
+        <input type="text" class="form-control" id="coordinate" name="coordinate" placeholder="coordinate..." required autocomplete="off">
+        <label for="coordinate">Coordinate</label>
+        <div class="invalid-feedback"></div>
+        </div>
+
+
+        <div class="row g-1 mb-1">
+        <div class="col">
+
+            <div class="border rounded px-1">
+                <div class="form-switch">
+                <input type="hidden"  value="0" name="is_active">
+                <input class="form-check-input" type="checkbox"  value="1" name="is_active" checked id="is_active">
+                <label class="form-check-label" for="is_active">Is Active</label>
+                </div>
+            </div>
+        </div>
+
+
+        </div>
+
+
+    </div>
+
+	<div class="card-footer text-end">
+      <a class="btn btn-outline-success ajax">Save</a>
+	</div>
+	</form>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'create_dormitory',
+}
+</script>
---
+++ src/views/panel/Hospitality/Hospitalities/Hospitalities.vue
@@ -0,0 +1,86 @@
+<template>
+  <CRow>
+    <CCol :xs="12">
+      <CCard class="mb-4">
+        <CCardHeader>
+          <div class="row">
+            <div class="col-md-10">
+              <strong>Hospitalities</strong>
+            </div>
+            <div class="col-md-2">
+              <router-link
+                to="/hospitalities/create"
+              >
+                <CButton
+                  color="primary"
+                >
+                  Create Hospitality
+                </CButton>
+              </router-link>
+            </div>
+          </div>
+        </CCardHeader>
+        <CCardBody>
+            <CTable>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Required Date</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Quantity</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Received By</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Extra</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                  <CTableDataCell>Mark</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>
+                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                  </CTableDataCell>
+                </CTableRow>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                  <CTableDataCell>Mark</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>
+                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                  </CTableDataCell>
+                </CTableRow>
+                <CTableRow>
+                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
+                  <CTableDataCell>Mark</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>Otto</CTableDataCell>
+                  <CTableDataCell>
+                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
+                    <button class="btn btn-sm btn-danger text-white">Delete</button>
+                  </CTableDataCell>
+                </CTableRow>
+              </CTableBody>
+            </CTable>
+        </CCardBody>
+      </CCard>
+    </CCol>
+  </CRow>
+</template>
+
+<script>
+export default {
+  name: 'Hospitalities',
+}
+</script>
