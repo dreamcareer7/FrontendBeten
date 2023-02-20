c883645fc41a3a90b6dbe0a32101582d71df5adb
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-21 03:42:18 +0500

    Documents incomplete

--- src/views/panel/Document/CreateDocument.vue
+++ src/views/panel/Document/CreateDocument.vue
@@ -8,32 +8,60 @@
 	<form method="post">
         <div class="card-body">
 
-            <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="title" name="title" placeholder="Title..." required autofocus autocomplete="off">
-            <label for="title">Title</label>
-            <div class="invalid-feedback"></div>
+          <div class="row">
+            <div class="col-6">
+              <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="title" name="title" placeholder="Title..." required autofocus autocomplete="off">
+                <label for="title">Title</label>
+                <div class="invalid-feedback"></div>
+              </div>
+              <div class="form-floating mb-3">
+                <select
+                  class="form-control"
+
+                  >
+                  <option value="dormitory">Dormitory</option>
+                  <option value="client">Client</option>
+                  <option value="vehicle">Vehicle</option>
+                  <option value="crew">Crew</option>
+                </select>
+                <label for="path">Belongs to</label>
+                <div class="invalid-feedback"></div>
+              </div>
+              <div class="form-floating mb-3">
+                <select
+                  class="form-control"
+                  >
+                  <option value="misc">Misc</option>
+                  <option value="nic">Identity</option>
+                  <option value="driving_license">Driving License</option>
+                  <option value="driving_license">Driving License</option>
+
+                </select>
+                <label for="path">Document Type</label>
+                <div class="invalid-feedback"></div>
+              </div>
+               <div class="form-floating mb-3">
+                <input type="text" class="form-control" id="model_id" name="model_id" placeholder="Model ID..." required autocomplete="off">
+                <label for="model_id">Model ID</label>
+                <div class="invalid-feedback"></div>
+              </div>
+
             </div>
+            <div class="col-6">
 
-
-            <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="path" name="path" placeholder="Path..." required autocomplete="off">
-            <label for="path">Path</label>
-            <div class="invalid-feedback"></div>
             </div>
-
-            <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="model_type" name="model_type" placeholder="Model Type..." required autocomplete="off">
-            <label for="model_type">Model Type</label>
-            <div class="invalid-feedback"></div>
-            </div>
-
-            <div class="form-floating mb-3">
-            <input type="text" class="form-control" id="model_id" name="model_id" placeholder="Model ID..." required autocomplete="off">
-            <label for="model_id">Model ID</label>
-            <div class="invalid-feedback"></div>
-            </div>
-
-
+          </div>
+          <CRow>
+            <CCol :md="12">
+              <div v-show="message && !success" class="error_style">
+                {{ message }}
+              </div>
+              <div v-show="message && success" class="alert alert-success">
+                {{ message }}
+              </div>
+            </CCol>
+          </CRow>
         </div>
 
 	<div class="card-footer text-end">
--- src/views/panel/Document/Documents.vue
+++ src/views/panel/Document/Documents.vue
@@ -9,68 +9,164 @@
             </div>
             <div class="col-md-2">
               <router-link
-                to="/documents/create"
+                :to="{
+                  name: 'Create Document',
+                }"
               >
-                <CButton
-                  color="primary"
-                >
-                  Create Document
-                </CButton>
+                <CButton color="primary"> Upload Document </CButton>
               </router-link>
             </div>
           </div>
         </CCardHeader>
         <CCardBody>
-            <CTable>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Title</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Path</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Model Type</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Model ID</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-success text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-success text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-success text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-              </CTableBody>
-            </CTable>
+          <CRow>
+            <CCol :md="2">
+              <select
+                type="text"
+                class="form-control"
+                v-model="search.country"
+                 @change="getClients"
+              >
+              <option value="" selected disabled>Country</option>
+                <template v-for="country in countries" :key="country.code">
+                  <option :value="country.code">{{ country.name }}</option>
+                </template>
+              </select>
+            </CCol>
+            <CCol :md="2">
+              <select
+                type="text"
+                class="form-control"
+                v-model="search.gender"
+                 @change="getClients"
+              >
+              <option value="" selected disabled>Gender</option>
+              <option value="1">Male</option>
+              <option value="0">Female</option>
+              </select>
+            </CCol>
+            <CCol :md="2">
+              <input
+                type="text"
+                class="form-control"
+                v-model="search.name"
+                placeholder="Name"
+                @change="getClients"
+              />
+            </CCol>
+            <CCol :md="2">
+              <input
+                type="text"
+                class="form-control"
+                v-model="search.phone"
+                placeholder="Phone"
+                @change="getClients"
+              />
+            </CCol>
+            <CCol :md="2">
+              <input
+                type="text"
+                class="form-control"
+                v-model="search.id_no"
+                placeholder="Id Number"
+                @change="getClients"
+              />
+            </CCol>
+          </CRow>
+          <CRow v-if="loading" class="mt-4">
+            <CCol :md="12" class="text-center">
+              <div class="spinner-border text-success" role="status"></div>
+            </CCol>
+            <CCol :md="12" class="text-center">
+              <span class="sr-only">Loading...</span>
+            </CCol>
+          </CRow>
+          <CTable v-if="!loading">
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Name</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Gender</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Country</CTableHeaderCell>
+                <CTableHeaderCell scope="col">phone</CTableHeaderCell>
+                <CTableHeaderCell scope="col">ID Type</CTableHeaderCell>
+                <CTableHeaderCell scope="col">ID No</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Date Of Birth</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow v-for="client in clients" :key="client.id">
+                <CTableHeaderCell scope="row">{{ client.id }}</CTableHeaderCell>
+                <CTableDataCell>{{ client.fullname }}</CTableDataCell>
+                <CTableDataCell>
+                  <span v-if="client.gender === 1">Male</span>
+                  <span v-if="client.gender === 0">Female</span>
+                </CTableDataCell>
+                <CTableDataCell>{{ client.country_id }}</CTableDataCell>
+                <CTableDataCell>{{ client.phone }}</CTableDataCell>
+                <CTableDataCell>{{ client.id_type }}</CTableDataCell>
+                <CTableDataCell>{{ client.id_no }}</CTableDataCell>
+                <CTableDataCell>{{ client.dob }}</CTableDataCell>
+                <CTableDataCell>
+                  <router-link
+                    :to="{
+                      name: 'user_info',
+                      params: { id: client.id },
+                    }"
+                  >
+                    <button
+                      style="margin-right: 1em"
+                      class="btn btn-sm btn-info text-white"
+                    >
+                      View
+                    </button>
+                  </router-link>
+                  <router-link
+                    :to="{
+                      name: 'update_client',
+                      params: { id: client.id },
+                    }"
+                  >
+                    <CButton class="btn btn-success text-white">Update</CButton>
+                  </router-link>
+                  <button
+                    class="btn btn-danger text-white"
+                    @click="deleteClient(client.id)"
+                  >
+                    Delete
+                  </button>
+                </CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
+          <CRow>
+            <CCol :md="12" class="text-center">
+              <nav aria-label="Page navigation example">
+                <ul class="pagination">
+                  <li class="page-item" v-if="current_page > 1">
+                    <a class="page-link" href="#" @click.prevent="previousPage"
+                      >Previous</a
+                    >
+                  </li>
+                  <li class="page-item" v-for="page in pagination" :key="page">
+                    <a
+                      @click.prevent="gotoPage(page.url)"
+                      class="page-link"
+                      href="#"
+                      v-html="page.label"
+                      v-if="page.url"
+                    ></a>
+                  </li>
+                  <li class="page-item" v-if="last_page > current_page">
+                    <a class="page-link" href="#" @click.prevent="nextPage">
+                      Next</a
+                    >
+                  </li>
+                </ul>
+              </nav>
+            </CCol>
+          </CRow>
         </CCardBody>
       </CCard>
     </CCol>
@@ -78,7 +174,76 @@
 </template>
 
 <script>
+import axios from 'axios'
+import countries from "@/store/countries";
+
 export default {
-  name: 'documents',
+  name: 'clients',
+  data() {
+    return {
+      countries,
+      documents: {},
+      search: {
+        gender:'',
+        country:''
+       },
+      current_page: 1,
+      last_page: 99,
+      selected_user: null,
+      loading: false,
+      pagination: {},
+    }
+  },
+  mounted() {
+    this.getDocuments()
+  },
+  methods: {
+    nextPage: async function () {
+      this.current_page = this.current_page + 1
+      this.search.page = this.current_page
+      await this.getDocuments()
+    },
+    previousPage: async function () {
+      this.current_page = this.current_page - 1
+      this.search.page = this.current_page
+      await this.getDocuments()
+    },
+    getDocuments: async function () {
+      this.loading = true
+      await axios
+        .get(`/documents/paginate`, {
+          params: this.search,
+        })
+        .then((response) => {
+          this.documents = response.data.data
+          this.current_page = response.data.current_page
+          this.last_page = response.data.last_page
+          this.total_pages = response.data.total / response.data.per_page
+          this.pagination = response.data.links
+        })
+      this.loading = false
+    },
+    gotoPage: async function (url) {
+      this.loading = true
+      await axios
+        .get(url, {
+          params: this.search,
+        })
+        .then((response) => {
+          this.documents = response.data.data
+          this.current_page = response.data.current_page
+          this.last_page = response.data.last_page
+          this.total_pages = response.data.total / response.data.per_page
+          this.pagination = response.data.links
+        })
+      this.loading = false
+    },
+    deleteClient: async function (id) {
+      await axios.post(`/documents/delete/` + id).then((response) => {
+        alert(response.data.message)
+        this.getDocuments()
+      })
+    },
+  },
 }
 </script>
