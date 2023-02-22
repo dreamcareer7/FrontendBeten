3633d43bf5d1f91d0de6c1326072d1957a7ce1ea
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-23 00:04:46 +0100

    Refactor & format
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Service/Services/Services.vue
+++ src/views/panel/Service/Services/Services.vue
@@ -8,13 +8,8 @@
               <strong>Services</strong>
             </div>
             <div class="col-md-2">
-              <router-link
-                to="/services/create"
-              >
-                <CButton
-                  color="primary"
-                  class="float-end"
-                >
+              <router-link to="/services/create">
+                <CButton color="primary" class="float-end">
                   Create Service
                 </CButton>
               </router-link>
@@ -22,58 +17,68 @@
           </div>
         </CCardHeader>
         <CCardBody>
-            <CTable>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Title</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Country ID</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Before Date</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Exact Date</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">After Date</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                <CTableRow v-for="service in services" :key="service.id">
-                  <CTableHeaderCell scope="row">{{ service.id }}</CTableHeaderCell>
-                  <CTableDataCell>{{ service.title }}</CTableDataCell>
-                  <CTableDataCell>{{ service.country_name }}</CTableDataCell>
-                  <CTableDataCell>{{ service.before_date }}</CTableDataCell>
-                  <CTableDataCell>{{ service.exact_date }}</CTableDataCell>
-                  <CTableDataCell>{{ service.after_date }}</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-warning text-white">Update</button>
-                    <button @click="deleteService(service.id)" class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-              </CTableBody>
-            </CTable>
+          <CTable>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Country ID</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Before Date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Exact Date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">After Date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow v-for="service in services" :key="service.id">
+                <CTableHeaderCell scope="row">
+                  {{ service.id }}
+                </CTableHeaderCell>
+                <CTableDataCell>{{ service.title }}</CTableDataCell>
+                <CTableDataCell>{{ service.country_name }}</CTableDataCell>
+                <CTableDataCell>{{ service.before_date }}</CTableDataCell>
+                <CTableDataCell>{{ service.exact_date }}</CTableDataCell>
+                <CTableDataCell>{{ service.after_date }}</CTableDataCell>
+                <CTableDataCell>
+                  <button
+                    style="margin-right: 1em"
+                    class="btn btn-sm btn-info text-white"
+                  >
+                    View
+                  </button>
+                  <button
+                    style="margin-right: 1em"
+                    class="btn btn-sm btn-warning text-white"
+                  >
+                    Update
+                  </button>
+                  <button
+                    @click="deleteService(service.id)"
+                    class="btn btn-sm btn-danger text-white"
+                  >
+                    Delete
+                  </button>
+                </CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
         </CCardBody>
       </CCard>
     </CCol>
   </CRow>
 </template>
 
-
 <script>
-import axios from 'axios'
 export default {
-  name: 'services',
-  data() {
-    return {
-      services: [],
-      current_page: '',
-      selected_user: null,
-    }
-  },
-  mounted() {
-    this.getServices()
-  },
+  name: 'Services',
+  data: () => ({
+    services: [],
+    current_page: '',
+    selected_user: null,
+  }),
   methods: {
     getServices: async function () {
-      await axios.get(`/services`).then((response) => {
+      await this.$axios.get(`/services`).then((response) => {
         let allServices = Object.entries(response.data.data.data)
         allServices.forEach((s) => this.services.push(s[1]))
       })
@@ -87,18 +92,21 @@ export default {
         dangerMode: true,
       }).then((willDelete) => {
         if (willDelete) {
-          axios.delete(`/services/${id}`).then(() => this.getServices())
+          this.$axios.delete(`/services/${id}`).then(() => this.getServices())
           swal('Service has been deleted!', {
             icon: 'success',
-          });
+          })
         }
-      });
+      })
     },
-    fetchServiceInfo: async function(id) {
-      await axios.get(`/services/show/` + id).then((response) => {
+    fetchServiceInfo: async function (id) {
+      await this.$axios.get(`/services/show/` + id).then((response) => {
         console.log(response.data)
       })
     },
+  },
+  mounted() {
+    this.getServices()
   },
 }
 </script>
