7ff142223934478731e687c3e69bd3b3745ce72a
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-23 20:44:57 +0100

    Create hospitality
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Hospitality/Hospitalities/CreateHospitality.vue
+++ src/views/panel/Hospitality/Hospitalities/CreateHospitality.vue
@@ -1,66 +1,166 @@
 <template>
-    <div class="card border-success mb-4">
-        <div class="card-header">
-            Create Hospitality
+  <div class="card border-success mb-4">
+    <div class="card-header">Create hospitality</div>
+    <form @submit.prevent="createHospitality">
+      <div class="card-body">
+        <div class="form-floating mb-3">
+          <input
+            type="text"
+            class="form-control"
+            id="title"
+            required
+            autofocus
+            autocomplete="off"
+            v-model="hospitality.title"
+            :disabled="disabled"
+          />
+          <label for="title">Title *</label>
         </div>
 
-        <div id="ialert" class="" role="alert"></div>
-        <form method="post">
-            <div class="card-body">
+        <div class="form-floating mb-3">
+          <textarea
+            class="form-control"
+            id="description"
+            required
+            autocomplete="off"
+            v-model="hospitality.description"
+            :disabled="disabled"
+          ></textarea>
+          <label for="description">Description *</label>
+        </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="title" name="title" placeholder="Title..." required
-                        autofocus autocomplete="off">
-                    <label for="title">Title</label>
-                    <div class="invalid-feedback"></div>
-                </div>
+        <div class="form-floating mb-3">
+          <input
+            type="date"
+            class="form-control"
+            id="required_date"
+            required
+            v-model="hospitality.required_date"
+            :disabled="disabled"
+          />
+          <label for="required_date">Required date *</label>
+        </div>
 
-                <div class="form-floating mb-3">
-                    <textarea class="form-control" id="description" name="description" placeholder="Description..." required
-                        autocomplete="off"></textarea>
-                    <label for="description">Description</label>
-                    <div class="invalid-feedback"></div>
-                </div>
+        <div class="form-floating mb-3">
+          <input
+            type="number"
+            min="1"
+            class="form-control"
+            id="quantity"
+            required
+            autocomplete="off"
+            v-model="hospitality.quantity"
+            :disabled="disabled"
+          />
+          <label for="quantity">Quantity *</label>
+        </div>
 
-                <div class="form-floating mb-3">
-                    <input type="date" class="form-control" id="before_date" name="before_date"
-                        placeholder="Before date...">
-                    <label for="phone">Before date</label>
-                    <div class="invalid-feedback"></div>
-                </div>
+        <div class="form-floating mb-3">
+          <select
+            id="received_by"
+            class="form-select"
+            required
+            v-model="hospitality.received_by"
+            :disabled="disabled"
+          >
+            <option
+              v-for="receiver in receivers"
+              :value="receiver.id"
+              :key="receiver.id"
+            >
+              {{ receiver.fullname }}
+            </option>
+          </select>
+          <label for="received_by">Received by *</label>
+        </div>
 
-                <div class="form-floating mb-3">
-                    <input type="number" class="form-control" id="quantity" name="quantity" placeholder="Quantity..."
-                        required autocomplete="off">
-                    <label for="quantity">Quantity</label>
-                    <div class="invalid-feedback"></div>
-                </div>
+        <div class="form-floating mb-3">
+          <textarea
+            id="extra"
+            class="form-control"
+            v-model="hospitality.extra"
+            :disabled="disabled"
+          ></textarea>
+          <label for="extra">Extra information about the hospitality</label>
+        </div>
 
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="received_by" name="received_by" placeholder="Received By..."
-                        required autocomplete="off">
-                    <label for="received_by">Received By</label>
-                    <div class="invalid-feedback"></div>
-                </div>
-
-                <div class="form-floating mb-3">
-                    <input type="text" class="form-control" id="extra" name="extra" placeholder="Extra..." required
-                        autocomplete="off">
-                    <label for="extra">Extra</label>
-                    <div class="invalid-feedback"></div>
-                </div>
-
+        <CRow>
+          <CCol :md="12">
+            <div
+              v-show="message"
+              class="alert"
+              :class="{ 'alert-success': success, error_style: !success }"
+            >
+              {{ message }}
             </div>
-
-            <div class="card-footer text-end">
-                <a class="btn btn-outline-success ajax">Save</a>
+            <div v-show="receivers.length === 0" class="alert alert-warning">
+              There are no crew members to receive hospitalities, please
+              <router-link :to="{ name: 'Create_Crew' }"
+                >create one</router-link
+              >
+              first
             </div>
-        </form>
-    </div>
+          </CCol>
+        </CRow>
+      </div>
+
+      <div class="card-footer text-end">
+        <span class="float-start">* Required fields</span>
+        <button
+          class="btn btn-success text-white"
+          type="submit"
+          :disabled="disabled"
+        >
+          Create hospitality
+        </button>
+      </div>
+    </form>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'create_hospitality',
+  name: 'CreateHospitality',
+  data: () => ({
+    message: '',
+    success: false,
+    hospitality: {},
+    receivers: [],
+    disabled: false,
+  }),
+  methods: {
+    createHospitality() {
+      this.$axios
+        .post('/hospitalities', this.hospitality)
+        .then((response) => {
+          if (response.status === 201) {
+            // Created
+            this.message = 'Hospitality created'
+            this.success = true
+            // Reset the main data object for next creation
+            this.hospitality = {}
+          } else {
+            this.message = response.data.message
+            this.success = false
+          }
+        })
+        .catch((error) => {
+          if (error.response) {
+            this.message = error.response.data.message
+          } else {
+            this.message = error.message
+          }
+          this.success = false
+        })
+    },
+  },
+  async mounted() {
+    await this.$axios.get('/crews/list').then((response) => {
+      if (response.data.length === 0) {
+        this.disabled = true // Disable the entire form
+      }
+      this.receivers = response.data
+    })
+  },
 }
 </script>
