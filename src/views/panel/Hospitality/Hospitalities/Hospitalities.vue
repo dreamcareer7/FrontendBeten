69c5e993b7fbd60dfa757ede51b243a430b15305
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-23 21:20:46 +0100

    List & delete hospitalities
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Hospitality/Hospitalities/Hospitalities.vue
+++ src/views/panel/Hospitality/Hospitalities/Hospitalities.vue
@@ -8,13 +8,8 @@
               <strong>Hospitalities</strong>
             </div>
             <div class="col-md-2">
-              <router-link
-                to="/hospitalities/create"
-              >
-                <CButton
-                  color="primary"
-                  class="float-end"
-                >
+              <router-link to="/hospitalities/create">
+                <CButton color="primary" class="float-end">
                   Create Hospitality
                 </CButton>
               </router-link>
@@ -22,64 +17,47 @@
           </div>
         </CCardHeader>
         <CCardBody>
-            <CTable>
-              <CTableHead>
-                <CTableRow>
-                  <CTableHeaderCell scope="col">#</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Title</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Description</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Required Date</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Quantity</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Received By</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Extra</CTableHeaderCell>
-                  <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
-                </CTableRow>
-              </CTableHead>
-              <CTableBody>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-warning text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-warning text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-                <CTableRow>
-                  <CTableHeaderCell scope="row">1</CTableHeaderCell>
-                  <CTableDataCell>Mark</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>Otto</CTableDataCell>
-                  <CTableDataCell>
-                    <button style="margin-right: 1em;" class="btn btn-sm btn-info text-white">View</button>
-<button style="margin-right: 1em;" class="btn btn-sm btn-warning text-white">Update</button>
-                    <button class="btn btn-sm btn-danger text-white">Delete</button>
-                  </CTableDataCell>
-                </CTableRow>
-              </CTableBody>
-            </CTable>
+          <CTable>
+            <CTableHead>
+              <CTableRow>
+                <CTableHeaderCell scope="col">#</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Title</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Description</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Required date</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Quantity</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Received by</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Extra</CTableHeaderCell>
+                <CTableHeaderCell scope="col">Actions</CTableHeaderCell>
+              </CTableRow>
+            </CTableHead>
+            <CTableBody>
+              <CTableRow
+                v-for="hospitality in hospitalities"
+                :key="hospitality.id"
+              >
+                <CTableHeaderCell scope="row">
+                  {{ hospitality.id }}
+                </CTableHeaderCell>
+                <CTableDataCell>{{ hospitality.title }}</CTableDataCell>
+                <CTableDataCell>{{ hospitality.description }}</CTableDataCell>
+                <CTableDataCell>{{ hospitality.required_date }}</CTableDataCell>
+                <CTableDataCell>{{ hospitality.quantity }}</CTableDataCell>
+                <CTableDataCell>
+                  {{ hospitality.receiver.fullname }}
+                </CTableDataCell>
+                <CTableDataCell>{{ hospitality.extra }}</CTableDataCell>
+                <CTableDataCell :aria-colspan="2">
+                  <button
+                    class="btn btn-sm btn-danger text-white"
+                    @click="deleteHospitality(hospitality.id)"
+                    title="Delete"
+                  >
+                    <ion-icon name="trash-bin-outline"></ion-icon>
+                  </button>
+                </CTableDataCell>
+              </CTableRow>
+            </CTableBody>
+          </CTable>
         </CCardBody>
       </CCard>
     </CCol>
@@ -89,5 +67,38 @@
 <script>
 export default {
   name: 'Hospitalities',
+  data: () => ({
+    hospitalities: [],
+  }),
+  methods: {
+    async deleteHospitality(id) {
+      await this.$axios
+        .delete(`/hospitalities/${id}`)
+        .then(() => {
+          this.hospitalities = this.hospitalities.filter(
+            (hospitality) => hospitality.id !== id,
+          )
+        })
+        .catch((error) => {
+          swal({
+            title: error.response.statusText,
+            text: error.response.data.message,
+            icon: 'error',
+          })
+        })
+    },
+  },
+  async mounted() {
+    await this.$axios
+      .get('/hospitalities')
+      .then((response) => (this.hospitalities = response.data))
+      .catch((error) => {
+        swal({
+          title: error.response.statusText,
+          text: error.response.data.message,
+          icon: 'error',
+        })
+      })
+  },
 }
 </script>
