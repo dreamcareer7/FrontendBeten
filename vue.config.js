b66a65a9ddebdd1ceca87f5fb92edda174dbc117
dreamcareer7 <dreamcareer070@gmail.com>
2023-02-24 18:00:11 +0100

    Correct loading of ion-icons
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- vue.config.js
+++ vue.config.js
@@ -4,4 +4,17 @@ module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
   publicPath: process.env.VUE_APP_PUBLIC_PATH,
+  runtimeCompiler: true,
+  chainWebpack: config => {
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap(options => {
+        options.compilerOptions = {
+          ...options.compilerOptions,
+          isCustomElement: tag => tag.startsWith('ion-')
+        }
+        return options
+      })
+  }
 })
