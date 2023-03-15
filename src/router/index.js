0d9bb2319cd5218359453cebb9c6eef98d3e10bc
dreamcareer7 <dreamcareer070@gmail.com>
2023-03-15 19:38:22 +0100

    [WIP] Rewriting concurrents
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/router/index.js
+++ src/router/index.js
@@ -208,21 +208,6 @@ const routes = [
         name: 'Update Dormitory',
         component: () => import('@/views/panel/Dormitory/UpdateDormitory'),
       },
-      {
-        path: '/concurrents',
-        name: 'concurrents',
-        component: () => import('@/views/panel/Concurrent/Concurrents'),
-      },
-      {
-        path: '/concurrents/create',
-        name: 'Create Concurrent',
-        component: () => import('@/views/panel/Concurrent/CreateConcurrent'),
-      },
-      {
-        path: '/concurrents/edit/:id',
-        name: 'Concurrents Edit',
-        component: () => import('@/views/panel/Concurrent/UpdateConcurrent'),
-      },
       // Hospitalities Menu
       {
         path: '/hospitalities',
