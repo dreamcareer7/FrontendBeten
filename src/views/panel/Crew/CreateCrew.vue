cb409ffb607f3ca3fc56e6b2d9f5c4e2302a8a5f
dreamcareer7 <dreamcareer070@gmail.com>
2023-03-17 16:08:22 +0100

    Fixed create client and crew
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Crew/CreateCrew.vue
+++ src/views/panel/Crew/CreateCrew.vue
@@ -1,249 +1,188 @@
 <template>
   <div class="card border-success mb-4">
-    <div class="card-header">Create Crew</div>
-
-    <div id="ialert" class="" role="alert"></div>
-    <form method="post">
+    <div class="card-header">Create crew</div>
+    <form @submit.prevent="create">
       <div class="card-body">
+        <div class="form-floating mb-3">
+          <select v-model="crew.user_id" id="user" class="form-control">
+            <option>Choose user</option>
+            <template v-for="user in data.users" :key="user.id">
+              <option :value="user.id">{{ user.name }}</option>
+            </template>
+          </select>
+          <label for="user">User</label>
+        </div>
+
         <div class="form-floating mb-3">
           <input
             v-model="crew.fullname"
             type="text"
             class="form-control"
-            id="fname"
-            name="fullname"
-            placeholder="Full Name..."
-            required
-            autofocus
+            id="fullname"
             autocomplete="off"
+            autofocus
+            required
           />
-          <label for="fname">Full Name</label>
-          <div class="invalid-feedback"></div>
+          <label for="fullname">Fullname *</label>
         </div>
 
         <div class="form-floating mb-3">
           <select
-            v-model="crew.gender"
-            name="gender"
+            class="form-control"
             id="gender"
-            class="form-control"
+            v-model="crew.gender"
+            required
           >
-            <option value="1">Male</option>
-            <option value="0">Female</option>
+            <option value="Male">Male</option>
+            <option value="Female">Female</option>
           </select>
-          <label for="gender">Gender</label>
-          <div class="invalid-feedback"></div>
-        </div>
-
-        <div class="form-floating mb-3">
-          <select
-            v-model="crew.country_id"
-            name="country"
-            id="country"
-            class="form-control"
-          >
-            <option>Choose Country</option>
-            <template v-for="country in countries" :key="country.code">
-              <option :value="country.id">{{ country.name }}</option>
-            </template>
-          </select>
-          <label for="country_id">Country</label>
-          <div class="invalid-feedback"></div>
+          <label for="gender">Gender *</label>
         </div>
 
         <div class="form-floating mb-3">
           <select
             v-model="crew.profession_id"
-            name="profession"
             id="profession"
             class="form-control"
           >
-            <option>Choose Profession</option>
-            <template v-for="profession in professions" :key="profession.id">
+            <option>Choose profession</option>
+            <template
+              v-for="profession in data.professions"
+              :key="profession.id"
+            >
               <option :value="profession.id">{{ profession.title }}</option>
             </template>
           </select>
-          <label for="profession_id">Profession</label>
-          <div class="invalid-feedback"></div>
+          <label for="profession">Profession</label>
         </div>
 
-        <div class="row g-1 mb-1">
-          <div class="col">
-            <div class="form-floating mb-3">
-              <input
-                v-model="crew.phone"
-                type="text"
-                class="form-control"
-                id="phone"
-                name="phone"
-                placeholder="Phone Number..."
-              />
-              <label for="phone">Phone Number</label>
-              <div class="invalid-feedback"></div>
-            </div>
+        <div class="form-floating mb-3">
+          <select
+            v-model="crew.country_id"
+            id="country"
+            class="form-control"
+            required
+          >
+            <option>Choose Country</option>
+            <template v-for="country in data.countries" :key="country.id">
+              <option :value="country.id">{{ country.title }}</option>
+            </template>
+          </select>
+          <label for="country">Country *</label>
+        </div>
 
-            <div class="form-floating mb-3">
-              <input
-                v-model="crew.id_type"
-                type="text"
-                class="form-control"
-                id="id_type"
-                name="id_type"
-                placeholder="ID Type..."
-              />
-              <label for="phone">ID Type</label>
-              <div class="invalid-feedback"></div>
-            </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="crew.phone"
+            type="text"
+            class="form-control"
+            id="phone"
+            required
+          />
+          <label for="phone">Phone number *</label>
+        </div>
 
-            <div class="form-floating mb-3">
-              <input
-                v-model="crew.id_number"
-                type="text"
-                class="form-control"
-                id="id_number"
-                name="id_number"
-                placeholder="ID Number..."
-              />
-              <label for="phone">ID Number</label>
-              <div class="invalid-feedback"></div>
-            </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="crew.id_type"
+            type="text"
+            class="form-control"
+            id="id_type"
+            required
+          />
+          <label for="id_type">ID type *</label>
+        </div>
 
-            <div class="form-floating mb-3">
-              <input
-                v-model="crew.dob"
-                type="date"
-                class="form-control"
-                id="dob"
-                name="dob"
-                placeholder="Date of Birth..."
-              />
-              <label for="phone">Date of Birth</label>
-              <div class="invalid-feedback"></div>
-            </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="crew.id_name"
+            type="text"
+            class="form-control"
+            id="id_name"
+            required
+          />
+          <label for="id_name">ID name *</label>
+        </div>
 
-            <div class="border rounded px-1">
-              <div class="form-switch">
-                <input
-                  class="form-check-input"
-                  type="checkbox"
-                  v-model="crew.is_handicap"
-                  name="is_handicap"
-                  checked
-                  id="is_handicap"
-                />
-                <label
-                  class="form-check-label"
-                  style="margin-left: 0.4em"
-                  for="is_handicap"
-                  >Is Handicap?</label
-                >
-              </div>
+        <div class="form-floating mb-3">
+          <input
+            v-model="crew.id_number"
+            type="text"
+            class="form-control"
+            id="id_number"
+            required
+          />
+          <label for="id_number">ID number</label>
+        </div>
+
+        <div class="form-floating mb-3">
+          <input
+            v-model="crew.dob"
+            type="date"
+            class="form-control"
+            id="dob"
+            required
+          />
+          <label for="dob">Date of birth *</label>
+        </div>
+
+        <div class="form-switch">
+          <input
+            class="form-check-input"
+            type="checkbox"
+            id="is_active"
+            v-model="crew.is_active"
+            required
+          />
+          <label class="form-check-label" for="is_active">
+            &nbsp;is active?
+          </label>
+        </div>
+
+        <div class="row" v-if="error_message">
+          <div class="col" :md="12">
+            <div class="error_style">
+              {{ error_message }}
             </div>
           </div>
         </div>
-        <CRow>
-          <CCol :md="12">
-            <div v-show="message && !success" class="error_style">
-              {{ message }}
-            </div>
-            <div v-show="message && success" class="alert alert-success">
-              {{ message }}
-            </div>
-          </CCol>
-        </CRow>
       </div>
 
       <div class="card-footer text-end">
-        <a @click="createCrew()" class="btn btn-success text-white">
-          <svg
-            clip-rule="evenodd"
-            class="button-icon"
-            fill-rule="evenodd"
-            stroke-linejoin="round"
-            stroke-miterlimit="2"
-            viewBox="0 0 24 24"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <path
-              d="m21 3.998c0-.478-.379-1-1-1h-16c-.62 0-1 .519-1 1v16c0 .621.52 1 1 1h16c.478 0 1-.379 1-1zm-16.5.5h15v15h-15zm6.75 6.752h-3.5c-.414 0-.75.336-.75.75s.336.75.75.75h3.5v3.5c0 .414.336.75.75.75s.75-.336.75-.75v-3.5h3.5c.414 0 .75-.336.75-.75s-.336-.75-.75-.75h-3.5v-3.5c0-.414-.336-.75-.75-.75s-.75.336-.75.75z"
-              fill-rule="nonzero"
-            />
-          </svg>
-          <span>Save</span>
-        </a>
+        <button class="btn text-white btn-success" type="submit">
+          <ion-icon name="person-add-outline"></ion-icon>&nbsp;Create
+        </button>
       </div>
     </form>
   </div>
 </template>
 
 <script>
-import countries from '@/store/countries'
-
 export default {
   name: 'CreateCrew',
-  data() {
-    return {
-      message: '',
-      success: false,
-      crew: {
-        fullname: '',
-        gender: '',
-        profession_id: '',
-        country_id: '',
-        phone: '',
-        id_type: '',
-        id_number: '',
-        dob: '',
-        is_handicap: false,
-      },
-      countries: [],
-      professions: [
-        {
-          id: 1,
-          title: 'Developer',
-        },
-        {
-          id: 2,
-          title: 'Doctor',
-        },
-        {
-          id: 3,
-          title: 'Engineer',
-        },
-        {
-          id: 4,
-          title: 'Accountant',
-        },
-      ],
-    }
-  },
+  data: () => ({
+    error_message: '',
+    crew: {
+      is_active: false,
+    },
+    data: [],
+  }),
   methods: {
-    createCrew: async function () {
-      let crew = this.crew
+    create: async function () {
       await this.$axios
-        .post(`/crews/add`, crew)
-        .then((response) => {
-          this.message = response.data.message
-          if (response.data.success) {
-            this.$router.push({name: 'crews'})
-          } else {
-            this.success = false
-          }
-        })
-        .catch((error) => {
-          if (error.response) {
-            this.message = error.response.data.message
-          } else {
-            this.message = error.message
-          }
-          this.success = false
-        })
+        .post('/crews', this.crew)
+        .then(() => this.$router.push({ name: 'Crews' }))
+        .catch(
+          (error) =>
+            (this.error_message =
+              error.response?.data.message || error.message),
+        )
     },
   },
-  mounted() {
-    countries.fetchCountries().then((countries) => {
-      this.countries = countries
-    })
+  mounted: async function () {
+    await this.$axios
+      .get('crews/create')
+      .then((response) => (this.data = response.data))
   },
 }
 </script>
