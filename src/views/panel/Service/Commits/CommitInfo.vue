d473c470e255ed23e1311fa9d4d2c1c40d94804b
dreamcareer7 <dreamcareer070@gmail.com>
2023-03-21 00:43:39 +0100

    Formatting
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Service/Commits/CommitInfo.vue
+++ src/views/panel/Service/Commits/CommitInfo.vue
@@ -52,66 +52,71 @@
         </CTable>
         <div v-if="service_commit_logs?.length">
           <CRow>
-          <CTable>
-          <CTableHead>
-            <CTableRow>
-              <CTableHeaderCell scope="col">Model type</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Model id</CTableHeaderCell>
-              <CTableHeaderCell scope="col">Role</CTableHeaderCell>
-            </CTableRow>
-          </CTableHead>
-          <CTableBody>
-            <CTableRow v-for="log in service_commit_logs">
-              <CTableDataCell>{{ log.model_type }}</CTableDataCell>
-              <CTableDataCell>{{ log.model_type }}</CTableDataCell>
-              <CTableDataCell>{{ log.role }}</CTableDataCell>
-            </CTableRow>
-          </CTableBody>
-          
-        </CTable>
+            <CTable>
+              <CTableHead>
+                <CTableRow>
+                  <CTableHeaderCell scope="col">Model type</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Model id</CTableHeaderCell>
+                  <CTableHeaderCell scope="col">Role</CTableHeaderCell>
+                </CTableRow>
+              </CTableHead>
+              <CTableBody>
+                <CTableRow v-for="log in service_commit_logs">
+                  <CTableDataCell>{{ log.model_type }}</CTableDataCell>
+                  <CTableDataCell>{{ log.model_type }}</CTableDataCell>
+                  <CTableDataCell>{{ log.role }}</CTableDataCell>
+                </CTableRow>
+              </CTableBody>
+            </CTable>
           </CRow>
           <CRow>
-        <CCol :md="12" class="text-center">
-          <nav aria-label="Service commits navigation">
-            <ul class="pagination">
-              <template v-for="page in pagination" :key="page">
-                <li class="page-item" :class="{ active: page.active }">
-                  <a @click.prevent="gotoPage(page.url)" class="page-link" :class="{ disabled: !page.url }"
-                    v-html="page.label"></a>
-                </li>
-              </template>
-            </ul>
-          </nav>
-        </CCol>
-      </CRow>
+            <CCol :md="12" class="text-center">
+              <nav aria-label="Service commits navigation">
+                <ul class="pagination">
+                  <template v-for="page in pagination" :key="page">
+                    <li class="page-item" :class="{ active: page.active }">
+                      <a
+                        @click.prevent="gotoPage(page.url)"
+                        class="page-link"
+                        :class="{ disabled: !page.url }"
+                        v-html="page.label"
+                      ></a>
+                    </li>
+                  </template>
+                </ul>
+              </nav>
+            </CCol>
+          </CRow>
         </div>
-        
+
         <button class="btn btn-primary" @click="show_add_log_form = true">
           Add logs
         </button>
-        <button class="btn btn-primary" v-if="service_commit.service_commit_log?.length" @click="initialize">
+        <button
+          class="btn btn-primary"
+          v-if="service_commit.service_commit_log?.length"
+          @click="initialize"
+        >
           Initialize
         </button>
         <CModal
-    size="sm"
-    :visible="show_add_log_form"
-    @close="show_add_log_form = false"
-    class="modal-popup-detail"
-    data-backdrop="static"
-    data-keyboard="false"
-  >
-    <CModalHeader>
-      <CModalTitle>Add log</CModalTitle>
-    </CModalHeader>
-    <CModalBody>
-      <CreateLog
-          :service_commit_id="service_commit.id"
-          @created="getCommit"
+          size="sm"
+          :visible="show_add_log_form"
+          @close="show_add_log_form = false"
+          class="modal-popup-detail"
+          data-backdrop="static"
+          data-keyboard="false"
         >
-        </CreateLog>
-    </CModalBody>
-  </CModal>
-        
+          <CModalHeader>
+            <CModalTitle>Add log</CModalTitle>
+          </CModalHeader>
+          <CModalBody>
+            <CreateLog
+              :service_commit_id="service_commit.id"
+              @created="getCommit"
+            />
+          </CModalBody>
+        </CModal>
       </CCol>
     </CRow>
   </div>
@@ -132,12 +137,10 @@ export default {
   }),
   methods: {
     gotoPage: async function (url) {
-      await this.$axios
-        .get(url)
-        .then((response) => {
-          this.service_commit_logs = response.data.logs.data
-          this.pagination = response.data.logs.links
-        })
+      await this.$axios.get(url).then((response) => {
+        this.service_commit_logs = response.data.logs.data
+        this.pagination = response.data.logs.links
+      })
     },
     getCommit() {
       this.$axios
@@ -149,9 +152,10 @@ export default {
         })
     },
     initialize() {
-      this.$axios.get(`/service_commits/initialize/${this.service_commit.id}`)
+      this.$axios
+        .get(`/service_commits/initialize/${this.service_commit.id}`)
         .then(() => this.getCommit())
-    }
+    },
   },
   mounted() {
     this.getCommit()
