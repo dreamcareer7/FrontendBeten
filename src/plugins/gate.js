67501122a2a14cb61464dbf29643308772df7174
dreamcareer7 <dreamcareer070@gmail.com>
2023-03-21 17:17:04 +0100

    Nullable user & permissions
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/plugins/gate.js
+++ src/plugins/gate.js
@@ -2,10 +2,10 @@ const user = JSON.parse(localStorage.getItem('user'))
 const permissions = JSON.parse(localStorage.getItem('permissions'))
 
 const can = (permission) => {
-  let admin = user.is_admin
+  let admin = user?.is_admin
   let wildcard = `${permission.split('.')[0]}.*`
   return (
-    admin || permissions.includes(wildcard) || permissions.includes(permission)
+    admin || permissions?.includes(wildcard) || permissions?.includes(permission)
   )
 }
 
