64ef543fbf425142cb0481dfc153ac3b95cd6482
dreamcareer7 <dreamcareer070@gmail.com>
2023-03-26 18:53:09 +0100

    remove the client assigning to the group from update and add it to the view in group
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Group/Groups.vue
+++ src/views/panel/Group/Groups.vue
@@ -3,14 +3,14 @@
     <CCol :xs="12">
       <CCard class="mb-4">
         <CCardHeader>
-          <CCardTitle>{{ $t('Groups') }}</CCardTitle>
+          <CCardTitle>{{ $t("Groups") }}</CCardTitle>
           <router-link
             :to="{ name: 'Create group' }"
             v-if="$can('groups.create')"
           >
             <CButton color="success" class="float-end text-white">
               <ion-icon name="people-outline"></ion-icon>&nbsp;
-              {{ $t('Create group') }}
+              {{ $t("Create group") }}
             </CButton>
           </router-link>
         </CCardHeader>
@@ -41,25 +41,29 @@
               <div class="spinner-border text-success" role="status"></div>
             </CCol>
             <CCol :md="12" class="text-center">
-              <span class="sr-only">{{ $t('Loading...') }}</span>
+              <span class="sr-only">{{ $t("Loading...") }}</span>
             </CCol>
           </CRow>
           <CRow v-if="groups.length === 0 && !loading" class="mt-4">
             <CCol :md="12" class="text-center">
-              <span class="sr-only">{{ $t('No results') }}</span>
+              <span class="sr-only">{{ $t("No results") }}</span>
             </CCol>
           </CRow>
           <CTable v-if="!loading && groups.length > 0">
             <CTableHead>
               <CTableRow>
-                <CTableHeaderCell scope="col">{{ $t('Title') }}</CTableHeaderCell>
-                <CTableHeaderCell scope="col">{{ $t('Crew member') }}</CTableHeaderCell>
+                <CTableHeaderCell scope="col">
+                  {{ $t("Title") }}
+                </CTableHeaderCell>
+                <CTableHeaderCell scope="col">
+                  {{ $t("Crew member") }}
+                </CTableHeaderCell>
                 <CTableHeaderCell
                   style="width: 20%"
                   scope="col"
                   :aria-colspan="2"
                 >
-                  {{ $t('Actions') }}
+                  {{ $t("Actions") }}
                 </CTableHeaderCell>
               </CTableRow>
             </CTableHead>
@@ -126,9 +130,9 @@
     </CCol>
   </CRow>
 
-  <!-- Small modal size unless documents or contracts -->
+  <!-- Small modal size unless documents or contracts or concurrents -->
   <CModal
-    :size="modalSize"
+    size="lg"
     :visible="is_group_modal_visible"
     @close="is_group_modal_visible = false"
     class="modal-popup-detail"
@@ -136,37 +140,84 @@
     data-keyboard="false"
   >
     <CModalHeader>
-      <CModalTitle>{{ $t('Group Information') }}</CModalTitle>
+      <CModalTitle>{{ $t("Group Information") }}</CModalTitle>
     </CModalHeader>
     <CModalBody>
       <CRow>
         <CCol :md="12">
           <CTable class="table table-responsive">
             <CTableRow>
-              <CTableDataCell>{{ $t('Title') }}: {{ group.title }}</CTableDataCell>
+              <CTableDataCell>
+                {{ $t("Title") }}: {{ group.title }}
+              </CTableDataCell>
             </CTableRow>
             <CTableRow>
               <CTableDataCell>
-                {{ $t('Crew member') }}: {{ group.crew?.fullname }}
+                {{ $t("Crew member") }}: {{ group.crew?.fullname }}
               </CTableDataCell>
             </CTableRow>
           </CTable>
         </CCol>
       </CRow>
+      <hr />
       <CRow>
-        <CCol :md="12">
-          <h3>{{ $t('Clients') }}</h3>
-          <CTable class="table table-responsive">
-            <CTableHead>
-              <CTableRow>
-                <CTableHeaderCell scope="col">{{ $t('Fullname') }}</CTableHeaderCell>
-              </CTableRow>
-            </CTableHead>
-            <CTableRow v-for="client in group.clients" :key="client.id">
-              <CTableDataCell>{{ client.fullname }}</CTableDataCell>
-            </CTableRow>
-          </CTable>
+        <CCol :md="12" class="text-center">
+          <div class="row">
+            <div class="col-6">
+              <h6>{{ $t("Search Client") }}</h6>
+              <input
+                type="text"
+                v-model="client_query"
+                class="form-control"
+                @input.prevent="searchClients"
+              />
+              <div class="row mt-3">
+                <div class="row border p-2 mb-2 mx-0" v-for="client in clients">
+                  <div class="col-8">
+                    <h6>{{ client.fullname }}</h6>
+                  </div>
+                  <div class="col-4">
+                    <button
+                      type="button"
+                      class="btn btn-primary"
+                      @click="addClientToGroup(client)"
+                    >
+                      <ion-icon name="arrow-forward-outline"></ion-icon>
+                    </button>
+                  </div>
+                </div>
+              </div>
+              <CRow v-if="loading_clients" class="mt-3">
+                <CCol :md="12" class="text-center">
+                  <div class="spinner-border text-success" role="status"></div>
+                </CCol>
+                <CCol :md="12" class="text-center">
+                  <span class="sr-only">{{ $t("Loading...") }}</span>
+                </CCol>
+              </CRow>
+            </div>
+            <div class="col-6">
+              <h6>{{ $t("Assigned Members to this Group") }}</h6>
+              <div class="row border p-2 mb-2 mx-0" v-for="client in group.clients">
+                <div class="col-8">
+                  <h6>{{ client.fullname }}</h6>
+                </div>
+                <div class="col-4">
+                  <button
+                    class="btn btn-primary"
+                    type="button"
+                    @click="removeClientFromGroup(client)"
+                  >
+                    <ion-icon name="arrow-back-outline"></ion-icon>
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
         </CCol>
+        <button class="btn btn-warning text-white" @click="update">
+          <ion-icon name="save-outline"></ion-icon>&nbsp;{{ $t("Save") }}
+        </button>
       </CRow>
       <Documentable v-if="group.is_documentable" type="group" :id="group.id" />
       <Contractable v-if="group.is_contractable" type="group" :id="group.id" />
@@ -176,39 +227,101 @@
 </template>
 
 <script>
-import { debounce } from '@/utils/helper'
+import { debounce } from "@/utils/helper";
 
 export default {
-  name: 'Groups',
+  name: "Groups",
   data: () => ({
     debounceFn: null,
     group: {},
+    client_query: "",
+    clients: [],
     groups: [],
     search: {},
     loading: false,
+    loading_clients: false,
     pagination: [],
     is_group_modal_visible: false,
   }),
-  computed: {
-    modalSize() {
-      if (this.group.is_contractable || this.group.is_documentable || this.group.is_concurrable) {
-        return 'lg'
-      }
-      return 'sm'
-    }
-  },
   methods: {
+    update: async function () {
+      await swal({
+        title: this.$i18n.t("Are you sure?"),
+        text: this.$i18n.t(
+          "Click confirm to update, this action is irreversible"
+        ),
+        icon: "warning",
+        buttons: [this.$i18n.t("Cancel"), this.$i18n.t("Confirm")],
+      }).then((willUpdate) => {
+        if (willUpdate) {
+          this.group.clients = this.group.clients.map((client) => client.id);
+          this.$axios
+            .patch(`/groups/${this.group.id}`, this.group)
+            .then(() => {
+              this.$router.push({ name: "Groups" });
+              swal(this.$i18n.t("Updated successfully!"), {
+                icon: "success",
+                timer: 3000,
+              });
+              this.clients = [];
+              this.client_query = "";
+            })
+            .catch(
+              (error) =>
+                (this.error_message =
+                  error.response?.data.message || error.message)
+            );
+        }
+      });
+    },
+    async addClientToGroup(client) {
+      // If client is not already added to the group
+      if (!this.group.clients.includes(client)) {
+        this.group.clients.push(client);
+      }
+      const index = this.clients.indexOf(client);
+      this.clients.splice(index, 1);
+    },
+    removeClientFromGroup(client) {
+      const index = this.group.clients.indexOf(client);
+      if (index > -1) {
+        // only splice array when item is found
+        this.group.clients.splice(index, 1); // 2nd parameter means remove one item only
+      }
+      if (!this.clients.includes(client)) {
+        this.clients.push(client);
+      }
+    },
+    searchClients: async function () {
+      if (this.client_query) {
+        this.loading_clients = true
+        await this.$axios
+          .get("/clients", {
+            params: { fullname: this.client_query, per_page: 6 },
+          })
+          .then((response) => {
+            this.clients = response.data.data.filter((client) => {
+              return !this.group.clients
+                .map((client) => client.id)
+                .includes(client.id);
+            });
+            this.loading_clients = false
+          });
+      } else {
+        this.clients = [];
+      }
+    },
     async getGroups(reset = false) {
-      this.loading = true
+      this.loading = true;
       await this.$axios
         .get(`/groups`, {
           params: reset ? {} : this.search,
         })
         .then((response) => {
-          this.groups = response.data.data
-          this.pagination = response.data.links
-          this.loading = false
-        })
+          this.groups = response.data.data;
+          this.pagination = response.data.links;
+          this.loading = false;
+        });
     },
     filter: async function (value, event) {
       if (
@@ -222,48 +335,48 @@ export default {
     },
     fetchGroupInfo: async function (id) {
       await this.$axios.get(`/groups/${id}`).then((response) => {
-        this.group = response.data
-        this.is_group_modal_visible = true
-      })
+        this.group = response.data;
+        this.is_group_modal_visible = true;
+      });
     },
     gotoPage: async function (url) {
-      this.loading = true
+      this.loading = true;
       await this.$axios
         .get(url, {
           params: this.search,
         })
         .then((response) => {
-          this.groups = response.data.data
-          this.pagination = response.data.links
-          this.loading = false
-        })
+          this.groups = response.data.data;
+          this.pagination = response.data.links;
+          this.loading = false;
+        });
     },
     deleteGroup: async function (id) {
       await swal({
-        title: this.$i18n.t('Are you sure?'),
-        text: this.$i18n.t('Once deleted, you will not be able to recover!'),
-        icon: 'warning',
-        buttons: [this.$i18n.t('Cancel'), this.$i18n.t('Confirm')],
+        title: this.$i18n.t("Are you sure?"),
+        text: this.$i18n.t("Once deleted, you will not be able to recover!"),
+        icon: "warning",
+        buttons: [this.$i18n.t("Cancel"), this.$i18n.t("Confirm")],
         dangerMode: true,
       }).then((willDelete) => {
         if (willDelete) {
           this.$axios.delete(`/groups/${id}`).then(
             this.groups.splice(
               this.groups.findIndex((group) => group.id === id),
-              1,
-            ),
-          )
-          swal(this.$i18n.t('Group has been deleted!'), {
-            icon: 'success',
+              1
+            )
+          );
+          swal(this.$i18n.t("Group has been deleted!"), {
+            icon: "success",
             timer: 3000,
-          })
+          });
         }
-      })
+      });
     },
   },
   async mounted() {
-    await this.getGroups()
-    this.debounceFn = debounce(() => this.getGroups(), 500)
+    await this.getGroups();
+    this.debounceFn = debounce(() => this.getGroups(), 500);
   },
-}
+};
 </script>
--- src/views/panel/Group/UpdateGroup.vue
+++ src/views/panel/Group/UpdateGroup.vue
@@ -1,9 +1,9 @@
 <template>
   <form class="card border-warning mb-4" @submit.prevent="update">
     <div class="card-header">{{ $t("Update Group") }}</div>
-    <div class="row mt-4">
-      <div class="col-4">
-        <div class="card-body">
+    <div class="card-body">
+      <div class="row mt-4">
+        <div class="col-4">
           <div class="form-floating mb-3">
             <input
               type="text"
@@ -16,64 +16,16 @@
             />
             <label for="title">{{ $t("Title") }}</label>
           </div>
-
+        </div>
+        <div class="col-4">
           <div class="form-floating mb-3">
             <select v-model="group.crew_id" id="crew_id" class="form-control">
               <option>{{ $t("Choose Crew") }}</option>
-              <template v-for="crew in crews" :key="crew.id">
+              <template v-for="crew in crew_members" :key="crew.id">
                 <option :value="crew.id">{{ crew.fullname }}</option>
               </template>
             </select>
             <label for="crew_id">{{ $t("Crew") }}</label>
-          </div>
-        </div>
-      </div>
-      <div class="col-8 text-center">
-        <h6>{{ $t("Assigned Members to this Group") }}</h6>
-        <div class="row">
-          <div class="col-4">
-            <h6>{{ $t("Search Client") }}</h6>
-            <input
-              type="text"
-              v-model="client_query"
-              class="form-control"
-              @input.prevent="searchClients"
-            />
-            <div class="row mt-3">
-              <div
-                class="row border p-2 mb-2"
-                v-for="client in searched_client"
-              >
-                <div class="col-8">
-                  <h6>{{ client.fullname }}</h6>
-                </div>
-                <div class="col-4">
-                  <button
-                    type="button"
-                    class="btn btn-primary"
-                    @click="addClientToGroup(client)"
-                  >
-                    <ion-icon name="arrow-forward-outline"></ion-icon>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="col-7">
-            <div class="row border p-2 mb-2" v-for="client in group.clients">
-              <div class="col-8">
-                <h6>{{ client.fullname }}</h6>
-              </div>
-              <div class="col-2">
-                <button
-                  class="btn btn-primary"
-                  type="button"
-                  @click="removeClientFromGroup(client)"
-                >
-                  <ion-icon name="arrow-back-outline"></ion-icon>
-                </button>
-              </div>
-            </div>
           </div>
         </div>
       </div>
@@ -104,45 +56,10 @@ export default {
   name: "UpdateGroup",
   data: () => ({
     error_message: "",
-    client_query: "",
-    searched_client: [],
     group: {},
-    crews: [],
+    crew_members: [],
   }),
   methods: {
-    searchClients: async function () {
-      if (this.client_query) {
-        await this.$axios
-          .get("/clients", {
-            params: { fullname: this.client_query, per_page: 10 },
-          })
-          .then((response) => {
-            this.searched_client = response.data.data.filter((client) => {
-              return !this.group.clients.map((client) => client.id).includes(client.id);
-            });
-          });
-      } else {
-        this.searched_client = [];
-      }
-    },
-    async addClientToGroup(client) {
-      // If client is not already added to the group
-      if (!this.group.clients.includes(client)) {
-        this.group.clients.push(client);
-      }
-      const index = this.searched_client.indexOf(client);
-      this.searched_client.splice(index, 1);
-    },
-    removeClientFromGroup(client) {
-      const index = this.group.clients.indexOf(client);
-      if (index > -1) {
-        // only splice array when item is found
-        this.group.clients.splice(index, 1); // 2nd parameter means remove one item only
-      }
-      if (!this.searched_client.includes(client)) {
-        this.searched_client.push(client);
-      }
-    },
     update: async function () {
       await swal({
         title: this.$i18n.t("Are you sure?"),
@@ -153,7 +70,6 @@ export default {
         buttons: [this.$i18n.t("Cancel"), this.$i18n.t("Confirm")],
       }).then((willUpdate) => {
         if (willUpdate) {
-          this.group.clients = this.group.clients.map((client) => client.id);
           this.$axios
             .patch(`/groups/${this.group.id}`, this.group)
             .then(() => {
@@ -163,13 +79,11 @@ export default {
                 timer: 3000,
               });
             })
-            .catch((error) => {
-              if (error.response) {
-                this.error_message = error.response.data.message;
-              } else {
-                this.error_message = error.message;
-              }
-            });
+            .catch(
+              (error) =>
+                (this.error_message =
+                  error.response?.data.message || error.message)
+            );
         }
       });
     },
@@ -179,7 +93,7 @@ export default {
       .get(`/groups/${this.$decrypt(this.$route.params.id)}/edit`)
       .then((response) => {
         this.group = response.data.group;
-        this.crews = response.data.crews;
+        this.crew_members = response.data.crew_members;
       });
   },
 };
