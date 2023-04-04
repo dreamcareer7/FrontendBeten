d3248657873087a13f89f68aa60d600795c59f10
dreamcareer7 <dreamcareer070@gmail.com>
2023-04-04 20:38:51 +0100

    Correct translation
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Meal/Meals/CreateMeal.vue
+++ src/views/panel/Meal/Meals/CreateMeal.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="card border-success mb-4">
-    <div class="card-header">{{ $t('Create Meal') }}</div>
+    <div class="card-header">{{ $t('Create meal') }}</div>
     <form @submit.prevent="create">
       <div class="card-body">
         <div class="form-floating mb-3">
