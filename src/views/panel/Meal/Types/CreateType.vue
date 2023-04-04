224ea77e02303ecb3f627859e13c3a5d175e6172
dreamcareer7 <dreamcareer070@gmail.com>
2023-04-04 22:35:02 +0100

    Create meal type
    Signed-off-by: Salim Djerbouh <13698160+CaddyDz@users.noreply.github.com>

--- src/views/panel/Meal/Types/CreateType.vue
+++ src/views/panel/Meal/Types/CreateType.vue
@@ -1,46 +1,68 @@
 <template>
-  <div class="card border-success mb-4">
-    <div class="card-header">Create Type</div>
-
-    <div id="ialert" class="" role="alert"></div>
-    <form method="post">
-      <div class="card-body">
-        <div class="form-floating mb-3">
-          <input
-            type="text"
-            class="form-control"
-            id="title"
-            name="title"
-            placeholder="Title..."
-            required
-            autocomplete="off"
-          />
-          <label for="title">Title</label>
-          <div class="invalid-feedback"></div>
-        </div>
-
-        <div class="form-floating mb-3">
-          <textarea
-            class="form-control"
-            id="description"
-            name="description"
-            placeholder="Description..."
-            autocomplete="off"
-          ></textarea>
-          <label for="description">Description</label>
-          <div class="invalid-feedback"></div>
-        </div>
+  <form class="card border-success mb-4" @submit.prevent="create">
+    <div class="card-header">{{ $t('Create meal type') }}</div>
+    <div class="card-body">
+      <div class="form-floating mb-3">
+        <input
+          type="text"
+          minlength="3"
+          maxlength="255"
+          class="form-control"
+          id="title"
+          autocomplete="off"
+          v-model="meal_type.title"
+          required
+        />
+        <label for="title">{{ $t('Title') }}</label>
       </div>
 
-      <div class="card-footer text-end">
-        <a class="btn btn-outline-success ajax">{{ $t('Save') }}</a>
+      <div class="form-floating mb-3">
+        <textarea
+          class="form-control"
+          minlength="5"
+          maxlength="255"
+          id="description"
+          autocomplete="off"
+          v-model="meal_type.description"
+        ></textarea>
+        <label for="description">{{ $t('Description') }}</label>
       </div>
-    </form>
-  </div>
+
+      <CRow v-if="error_message">
+        <CCol :md="12">
+          <div class="error_style">
+            {{ error_message }}
+          </div>
+        </CCol>
+      </CRow>
+    </div>
+
+    <div class="card-footer text-end">
+      <button class="btn btn-success text-white" type="submit">
+        <ion-icon name="create-outline"></ion-icon>&nbsp;{{ $t("Create") }}
+      </button>
+    </div>
+  </form>
 </template>
 
 <script>
 export default {
-  name: 'create_types',
-}
+  name: "CreateMealType",
+  data: () => ({
+    error_message: '',
+    meal_type: {}
+  }),
+  methods: {
+    create: async function () {
+      await this.$axios
+        .post('/meal_types', this.meal_type)
+        .then(() => this.$router.push({ name: 'Types' }))
+        .catch(
+          (error) =>
+            (this.error_message =
+              error.response?.data.message || error.message),
+        )
+    },
+  },
+};
 </script>
