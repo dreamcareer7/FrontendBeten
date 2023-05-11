e7b3b8991bcf0b6f45d32bc103b1ea1027449fd8
dreamcareer7 <dreamcareer070@gmail.com>
2023-05-11 06:49:43 -0400

    feat: roles management

--- src/views/panel/Role/UpdateRole.vue
+++ src/views/panel/Role/UpdateRole.vue
@@ -1,99 +1,34 @@
 <template>
   <div class="card border-warning mb-4">
-    <div class="card-header">Update role</div>
-    <form @submit.prevent="updaterole">
+    <div class="card-header">Update Role</div>
+    <form @submit.prevent="update">
       <div class="card-body">
         <div class="form-floating mb-3">
-          <input
-            class="form-control"
-            id="fullname"
-            type="text"
-            autocomplete="off"
-            v-model="role.fullname"
-            required
-            autofocus
-          />
-          <label for="fullname">Full Name</label>
+          <CTableRow class="form-control">
+            <CTableHeaderCell>{{ role.name }}</CTableHeaderCell>
+          </CTableRow>
         </div>
 
         <div class="form-floating mb-3">
-          <select
-            class="form-control"
-            id="country_id"
-            v-model="role.country_id"
-            required
-          >
-            <option>Choose Country</option>
-            <option
-              :value="country.id"
-              v-for="country in countries"
-              :key="country.id"
-            >
-              {{ country.name }}
-            </option>
-          </select>
-          <label for="country_id">Country</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <input
-            class="form-control"
-            id="id_type"
-            type="text"
-            v-model="role.id_type"
-            required
-          />
-          <label for="id_type">ID Type</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <input
-            class="form-control"
-            id="id_number"
-            type="text"
-            v-model="role.id_number"
-            required
-          />
-          <label for="id_number">ID Number</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <input
-            type="text"
-            class="form-control"
-            id="id_name"
-            v-model="role.id_name"
-            required
-          />
-          <label for="id_name">ID Name</label>
-        </div>
-
-        <div class="form-floating mb-3">
-          <select
-            id="gender"
-            class="form-control"
-            v-model="role.gender"
-            required
-          >
-            <option value="Male">{{ $t('Male') }}</option>
-            <option value="Female">{{ $t('Female') }}</option>
-          </select>
-          <label for="gender">Gender</label>
-        </div>
-
-        <div class="border rounded px-1">
-          <div class="form-switch">
-            <input
-              class="form-check-input"
-              id="is_handicap"
-              type="checkbox"
-              :checked="role.is_handicap"
-              v-model="role.is_handicap"
-            />
-            <label class="form-check-label" for="is_handicap">
-              &nbsp;is handicapped?
-            </label>
-          </div>
+          <CAccordion>
+            <CAccordionItem>
+              <CAccordionHeader>
+                Permissions
+              </CAccordionHeader>
+              <CAccordionBody>
+                <div class="d-flex flex-wrap">
+                  <CFormCheck 
+                    v-for="a_permission in role.available_permissions"
+                    class="permission-item"
+                    :id="a_permission.id"
+                    :label="a_permission.name"
+                    :checked="role.permissions.find(permission => permission.id === a_permission.id)"
+                    @change="(event) => permissionChanged(event.target.checked, a_permission)"
+                  />
+                </div>
+              </CAccordionBody>
+            </CAccordionItem>
+          </CAccordion>
         </div>
 
         <CRow v-if="error_message">
@@ -112,23 +47,25 @@
         >
           Go back</button
         >&nbsp;
-        <button type="submit" class="btn text-white btn-warning">{{ $t('Save') }}</button>
+        <button type="submit" class="btn btn-warning text-white">
+          Save
+          <ion-icon name="save"></ion-icon>
+        </button>
       </div>
     </form>
   </div>
 </template>
-<script>
-import countries from '@/store/countries'
 
+<script>
 export default {
-  name: 'Update role',
+  name: 'UpdateRole',
   data: () => ({
-    error_message: '',
     role: {},
-    countries: [],
+    permissions: [],
+    error_message: '',
   }),
   methods: {
-    updaterole: async function () {
+    update: async function () {
       await swal({
         title: this.$i18n.t('Are you sure?'),
         text: this.$i18n.t('Click confirm to update, this action is irreversible'),
@@ -137,28 +74,44 @@ export default {
       }).then((willUpdate) => {
         if (willUpdate) {
           this.$axios
-            .patch(`/roles/${this.role.id}`, this.role)
+            .patch(`/roles/${this.role.id}`, { permissions: this.permissions })
             .then(() => {
-              this.$router.push({ name: 'roles' })
+              this.$router.push({ name: 'All roles' })
               swal(this.$i18n.t('Updated successfully!'), {
                 icon: 'success',
                 timer: 3000,
               })
             })
-            .catch(
-              (error) =>
-                (this.error_message =
-                  error.response?.data.message || error.message),
-            )
+            .catch((error) => {
+              if (error.response) {
+                this.error_message = error.response.data.message
+              } else {
+                this.error_message = error.message
+              }
+            })
         }
       })
     },
+    permissionChanged: function (checked, permission) {
+      if (checked)
+        this.permissions.push(permission.id);
+      else {
+        const index = this.permissions.indexOf(permission.id);
+        this.permissions.splice(index, 1);
+      }
+    },
   },
   async mounted() {
-    countries.fetchCountries().then((countries) => (this.countries = countries))
     await this.$axios
-      .get(`/roles/${this.$decrypt(this.$route.params.id)}`)
+      .get(`/roles/${this.$decrypt(this.$route.params.id)}/edit`)
       .then((response) => (this.role = response.data))
+    this.permissions = this.role.permissions.map(permission => permission.id);
   },
 }
 </script>
+
+<style scoped>
+.permission-item {
+  margin-left: 20px;
+}
+</style>
